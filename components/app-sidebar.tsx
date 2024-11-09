ae2fd0d73e3e51aaeecf23fc766b30be6b76af34
Tristan Katwaroo <katwarot@sheridancollege.ca>
2024-11-09 00:07:07 -0500

    remove version switcher

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -1,7 +1,6 @@
 import * as React from "react"
 
 import { SearchForm } from "@/components/search-form"
-import { VersionSwitcher } from "@/components/version-switcher"
 import {
   Sidebar,
   SidebarContent,
@@ -151,10 +150,11 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
     <Sidebar {...props}>
       <SidebarHeader>
-        <VersionSwitcher
+        {/* <VersionSwitcher
           versions={data.versions}
           defaultVersion={data.versions[0]}
-        />
+        /> */}
+        App Logo
         <SearchForm />
       </SidebarHeader>
       <SidebarContent>
